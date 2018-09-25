06d7e0421dff335cec26d75827160e43d2643f30
stacytalbot <stacymtalbot@gmail.com>
2018-09-25 11:01:40 +0100

    Create all pages in the site and code up nav

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,25 @@
 class SiteController < ApplicationController
-  def index
-    @data = YAML.load(File.open("#{Rails.root}/lib/data/content/global.yml", 'r'))
+  before_action :load_global
+
+  def warmwater
+    
   end
+
+  def saltmarshes
+  end
+
+  def mangroves
+  end
+
+  def seagrasses
+  end
+
+  def coldwater
+  end
+
+  private
+    def load_global
+      @global = YAML.load(File.open("#{Rails.root}/lib/data/content/global.yml", 'r'))
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'site#index'
+  root to: 'site#warmwater'
+
+  get '/warmwater', to: 'site#warmwater'
+  get '/saltmarshes', to: 'site#saltmarshes'
+  get '/mangroves', to: 'site#mangroves'
+  get '/seagrasses', to: 'site#seagrasses'
+  get '/coldwater', to: 'site#coldwater'
 end
