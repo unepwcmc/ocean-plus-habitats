591f7a7729227f92b39f1920fd6aef21d99a8c98
stacytalbot <stacymtalbot@gmail.com>
2018-09-25 12:54:10 +0100

    Remove DS_Store, update gitignore and add partial for first graph component

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,19 +2,23 @@ class SiteController < ApplicationController
   before_action :load_global
 
   def warmwater
-    
+    @title = 'Warm-water corals'
   end
 
   def saltmarshes
+    @title = 'Saltmarshes'
   end
 
   def mangroves
+    @title = 'Mangroves'
   end
 
   def seagrasses
+    @title = 'Seagrasses'
   end
 
   def coldwater
+    @title = 'Cold-water corals'
   end
 
   private
