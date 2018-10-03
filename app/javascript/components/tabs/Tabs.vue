a0af4812d63e1ea2de241e764cba489b58449b7e
stacytalbot <stacymtalbot@gmail.com>
2018-10-03 17:07:04 +0100

    Add tabs component

---
+++ app/javascript/components/tabs/Tabs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul class="tabs__triggers ul-inline">
+      <li v-for="child, index in children" @click="triggerTab(child.id)" class="tab__trigger" :class="{ 'tab-active': child.isActive }">
+        {{ child.title }}
+      </li>
+    </ul>
+    
+    <slot></slot>
+  </div>  
+</template>
+
+<script>
+  import { eventHub } from '../../packs/application.js'
+
+  export default {
+    name: 'tabs',
+
+    data () {
+      return {
+        children: []
+      }
+    },
+
+    created () {
+      this.children = this.$children
+    },
+
+    methods: {
+      triggerTab (selectedId) {
+        this.children.forEach(child => {
+          child.isActive = child.id === selectedId
+        })
+      }
+    }
+  }
+</script>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,24 @@
-import Vue from 'vue'
+// libraries
+import Vue from 'vue/dist/vue.esm'
+import TurbolinksAdapter from 'vue-turbolinks'
+
+Vue.config.productionTip = false
+
+Vue.use(TurbolinksAdapter)
 
 // components
 import Tab from '../components/tabs/Tab.vue'
 import Tabs from '../components/tabs/Tabs.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const el = document.body.appendChild(document.createElement('hello'))
-  
+// create event hub and export so that it can be imported into .vue files
+export const eventHub = new Vue()
+
+document.addEventListener('turbolinks:load', () => {
   const app = new Vue({
-    el,
-    components: [
+    el: '#v-app',
+    components: {
       Tab,
       Tabs
-    ]
+    }
   })
 })
