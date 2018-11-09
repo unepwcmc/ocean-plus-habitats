ffdb4459c8a651501c7f1d3ec1f2bd66c28f508e
stacytalbot <stacymtalbot@gmail.com>
2018-11-09 16:19:27 +0000

    Dummy content for all pages, refactor styling for themes

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -72,6 +72,57 @@ class SiteController < ApplicationController
       @sdgs,
       @sdgs = YAML.load(File.open("#{Rails.root}/lib/data/content/meas-saltmarshes.yml", 'r'))
     ]
+
+    @chart_greatest_coverage = [
+      {
+        label: 'Australia',
+        value: '94',
+        percent: '94',
+      },
+      {
+        label: 'United Kingdom',
+        value: '63',
+        percent: '63',
+      },
+      {
+        label: 'Spain',
+        value: '75',
+        percent: '75',
+      },
+      {
+        label: 'Italy',
+        value: '50',
+        percent: '50',
+      },
+      {
+        label: 'Russia',
+        value: '10',
+        percent: '10',
+      }
+    ]
+
+    @chart_protected_areas = [
+      {
+        label: 'Australia',
+        percent: '94',
+      },
+      {
+        label: 'United Kingdom',
+        percent: '63',
+      },
+      {
+        label: 'Spain',
+        percent: '75',
+      },
+      {
+        label: 'Italy',
+        percent: '50',
+      },
+      {
+        label: 'Russia',
+        percent: '10',
+      }
+    ]
   end
 
   def mangroves
@@ -82,6 +133,57 @@ class SiteController < ApplicationController
       @aichi_targets,
       @sdgs,
       @sdgs = YAML.load(File.open("#{Rails.root}/lib/data/content/meas-mangroves.yml", 'r'))
+    ]
+
+    @chart_greatest_coverage = [
+      {
+        label: 'Australia',
+        value: '94',
+        percent: '94',
+      },
+      {
+        label: 'United Kingdom',
+        value: '63',
+        percent: '63',
+      },
+      {
+        label: 'Spain',
+        value: '75',
+        percent: '75',
+      },
+      {
+        label: 'Italy',
+        value: '50',
+        percent: '50',
+      },
+      {
+        label: 'Russia',
+        value: '10',
+        percent: '10',
+      }
+    ]
+
+    @chart_protected_areas = [
+      {
+        label: 'Australia',
+        percent: '94',
+      },
+      {
+        label: 'United Kingdom',
+        percent: '63',
+      },
+      {
+        label: 'Spain',
+        percent: '75',
+      },
+      {
+        label: 'Italy',
+        percent: '50',
+      },
+      {
+        label: 'Russia',
+        percent: '10',
+      }
     ]
   end
 
@@ -94,6 +196,57 @@ class SiteController < ApplicationController
       @sdgs,
       @sdgs = YAML.load(File.open("#{Rails.root}/lib/data/content/meas-seagrasses.yml", 'r'))
     ]
+
+    @chart_greatest_coverage = [
+      {
+        label: 'Australia',
+        value: '94',
+        percent: '94',
+      },
+      {
+        label: 'United Kingdom',
+        value: '63',
+        percent: '63',
+      },
+      {
+        label: 'Spain',
+        value: '75',
+        percent: '75',
+      },
+      {
+        label: 'Italy',
+        value: '50',
+        percent: '50',
+      },
+      {
+        label: 'Russia',
+        value: '10',
+        percent: '10',
+      }
+    ]
+
+    @chart_protected_areas = [
+      {
+        label: 'Australia',
+        percent: '94',
+      },
+      {
+        label: 'United Kingdom',
+        percent: '63',
+      },
+      {
+        label: 'Spain',
+        percent: '75',
+      },
+      {
+        label: 'Italy',
+        percent: '50',
+      },
+      {
+        label: 'Russia',
+        percent: '10',
+      }
+    ]
   end
 
   def coldwater
@@ -104,6 +257,57 @@ class SiteController < ApplicationController
       @aichi_targets,
       @sdgs,
       @sdgs = YAML.load(File.open("#{Rails.root}/lib/data/content/meas-coldwater.yml", 'r'))
+    ]
+
+    @chart_greatest_coverage = [
+      {
+        label: 'Australia',
+        value: '94',
+        percent: '94',
+      },
+      {
+        label: 'United Kingdom',
+        value: '63',
+        percent: '63',
+      },
+      {
+        label: 'Spain',
+        value: '75',
+        percent: '75',
+      },
+      {
+        label: 'Italy',
+        value: '50',
+        percent: '50',
+      },
+      {
+        label: 'Russia',
+        value: '10',
+        percent: '10',
+      }
+    ]
+
+    @chart_protected_areas = [
+      {
+        label: 'Australia',
+        percent: '94',
+      },
+      {
+        label: 'United Kingdom',
+        percent: '63',
+      },
+      {
+        label: 'Spain',
+        percent: '75',
+      },
+      {
+        label: 'Italy',
+        percent: '50',
+      },
+      {
+        label: 'Russia',
+        percent: '10',
+      }
     ]
   end
 
