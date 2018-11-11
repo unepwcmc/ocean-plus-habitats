804ec4bb13997ec67da3da986f43d24e5bc04e4d
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-11-11 22:22:31 +0000

    Simplify controller

--- app/models/habitat.rb
+++ app/models/habitat.rb
@@ -1,2 +1,9 @@
 class Habitat < ApplicationRecord
+  def global_coverage_title
+    "Total global coverage of #{title.downcase}"
+  end
+
+  def protected_title
+    "Percentage of #{title.downcase} that occur within a marine protected area"
+  end
 end
