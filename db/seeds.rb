8ed23428384ebd2c19c40d5e030121b79ebf5152
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-11-12 11:39:48 +0000

    Calculate global coverage for each habitat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@ habitats_config['habitats'].each do |name, data|
   Habitat.create(data)
   Rails.logger.info "#{name.capitalize} habitat created!"
 end
+
+Habitat.all.each do |habitat|
+  habitat.calculate_global_coverage
+  Rails.logger.info("Global coverage calculate for #{habitat.name}!")
+end
