2babff399c6c40e9d01f166015606a33524d74bf
Andrew Potter <hello@drewpotter.com>
2018-11-13 10:42:43 +0000

    Refactor the code and also remove areas where multiple iso are mentioned

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -56,19 +56,45 @@ class SiteController < ApplicationController
     @sdgs = YAML.load(File.open("#{Rails.root}/lib/data/content/sdgs.yml", 'r'))
   end
 
+  def calculate_percentage(global_area, country_total_area)
+    percentage_total_area = {}
+    country_total_area
+  end
+
+  def calculate_global_area(global_area)
+    total = 0.0
+    global_area.each do |global_area_habitat|
+      total = total + global_area_habitat.first["sum"].to_f
+    end
+    total
+  end
+
+  def sum_country_areas(habitat_data, country_total_area)
+    habitat_data.flatten.each do |country_data|
+      next if country_data["iso3"].include? "/" # remove areas which are multiple iso
+      country_total_area[country_data["iso3"]] = country_total_area[country_data["iso3"]].nil? ?  country_data["sum"] : country_total_area[country_data["iso3"]] + country_data["sum"]
+    end
+    country_total_area
+  end
+
   def load_charts_data
     habitat_data = []
+    global_area = []
     Habitat.all.each do |habitat|
       c = Carto.new(habitat.name)
       habitat_data << c.total_area_by_country
+      global_area << c.total_area
     end
 
     country_total_area = {}
-    habitat_data.flatten.each do |country_data|
-      country_total_area[country_data["iso3"]] = country_total_area[country_data["iso3"]].nil? ?  country_data["sum"] : country_total_area[country_data["iso3"]] + country_data["sum"]
-    end
 
-    country_total_area.sort_by {|_key, value| value}
+    country_total_area = sum_country_areas(habitat_data, country_total_area)
+    global_area = calculate_global_area(global_area)
+    country_total_area_percentage = calculate_percentage(global_area, country_total_area)
+
+    byebug
+    country_total_area_percentage.sort_by {|_key, value| value}
+    byebug
 
     @chart_greatest_coverage = [
       {
