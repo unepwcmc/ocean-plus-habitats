4773ae6b25da07ce062ce91d92dd9558fcaf58f8
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-11-13 11:37:50 +0000

    Add country model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181111204322) do
+ActiveRecord::Schema.define(version: 20181113112332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "countries", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "iso2", null: false
+    t.string "iso3", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "habitats", force: :cascade do |t|
     t.string "name", default: "", null: false
