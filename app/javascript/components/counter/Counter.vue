d15c7b9b05f71ccd809b9575b353768f920ef69e
stacytalbot <stacymtalbot@gmail.com>
2018-11-15 13:46:12 +0000

    Tidy up counter component

--- app/javascript/components/counter/Counter.vue
+++ app/javascript/components/counter/Counter.vue
@@ -34,38 +34,36 @@
     data () {
       return {
         currentNumber: 0,
-        // current: 0,
-        step: 0,
-        increase: true
+        step: 0
       }
     },
 
     computed: {
-      styledNumber: function () {
+      styledNumber () {
         const roundingNumber = 1
 
         return (Math.ceil(this.currentNumber * roundingNumber)/roundingNumber).toLocaleString()
       }
     },
 
-    created: function() {
+    created () {
       this.step = Math.abs(this.number - this.currentNumber) / this.divisor
     },
 
-    mounted: function() {
+    mounted () {
       this.scrollMagicHandlers()
     },
 
     methods: {
-      count: function () {
-        this.increase = this.currentNumber < this.number ? true : false
+      count () {
+        const increase = this.currentNumber < this.number ? true : false
 
-        var interval = window.setInterval(() => {
-          if(this.increase && this.currentNumber + this.step < this.number){
+        let interval = window.setInterval(() => {
+          if(increase && this.currentNumber + this.step < this.number){
               this.updateCurrentNumber(true)
 
-          } else if (!this.increase && this.currentNumber - this.step > this.number ){
-              this.updateCurrentNumber(true)
+          } else if (!increase && this.currentNumber - this.step > this.number ){
+              this.updateCurrentNumber(false)
 
           } else {
             this.currentNumber = this.number
@@ -78,7 +76,7 @@
         this.currentNumber = increment ? this.currentNumber + this.step : this.currentNumber - this.step
       },
 
-      scrollMagicHandlers: function () {
+      scrollMagicHandlers () {
         let scrollMagicCounter = new ScrollMagic.Controller()
 
         new ScrollMagic.Scene({ triggerElement: `.${this.smTrigger}`, reverse: false })
