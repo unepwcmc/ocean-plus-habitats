c7df4d01f80c581c5895c9f3146d3ef4ba232710
stacytalbot <stacymtalbot@gmail.com>
2018-12-18 11:24:45 +0000

    Remove old routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'site#index'
-
-  # get '/warmwater', to: 'site#warmwater'
-  # get '/saltmarshes', to: 'site#saltmarshes'
-  # get '/mangroves', to: 'site#mangroves'
-  # get '/seagrasses', to: 'site#seagrasses'
-  # get '/coldcorals', to: 'site#coldcorals'
 end
