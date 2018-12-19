d453855aa5d041cca29df403ab509e4632b86c7b
stacytalbot <stacymtalbot@gmail.com>
2018-12-19 11:02:29 +0000

    Moved out habitat data to a serializer

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,45 +1,16 @@
 class SiteController < ApplicationController
   respond_to? :json, :html
   before_action :load_habitat
-  before_action :load_global
   before_action :load_charts_data
 
   def index
+    @global = YAML.load(File.open("#{Rails.root}/lib/data/content/global.yml", 'r'))
+
     @title = @habitat.title
 
     @nav = @global['nav'].to_json
 
-    content = YAML.load(File.open("#{Rails.root}/lib/data/content/#{@habitat.name}.yml", 'r'))
-    aichi_targets = generateImageUrls @aichi_targets
-    sdgs = generateImageUrls @sdgs
-
-    @habitatData = {
-      name: @habitat.name,
-      theme: @habitat.theme,
-      nav: {
-
-      },
-      content: content,
-      map: {
-        habitatTitle: @habitat.title,
-        habitatType: @habitat_type,
-        theme: @habitat.theme,
-        tables: [@habitat.poly_table, @habitat.point_table].compact,
-        titleGlobal: @habitat.global_coverage_title(@habitat_type),
-        titleProtected: @habitat.protected_title,
-        percentageGlobal: @habitat.global_coverage,
-        percentageProtected: @habitat.protected_percentage,
-        wmsUrl: @habitat.wms_url
-      },
-      columnChart: @chart_greatest_coverage,
-      rowChart: @chart_protected_areas,
-      disclaimer: @global['disclaimer'],
-      commitments: [
-        aichi_targets, 
-        sdgs,
-        content['other_targets'] 
-      ]
-    }.to_json
+    @habitatData = HabitatsSerializer.new(@habitat, @chart_greatest_coverage, @chart_protected_areas, @global).serialize
 
     respond_to do |format|
       format.html
@@ -53,20 +24,6 @@ class SiteController < ApplicationController
     @habitat = Habitat.where(name: params['habitat'] || 'warmwater').first
     @habitat ||= Habitat.where(name: 'coralreef').first
     @habitat_type = @habitat.type
-  end
-
-  def load_global
-    @global = YAML.load(File.open("#{Rails.root}/lib/data/content/global.yml", 'r'))
-    @aichi_targets = YAML.load(File.open("#{Rails.root}/lib/data/content/aichi-targets.yml", 'r'))
-    @sdgs = YAML.load(File.open("#{Rails.root}/lib/data/content/sdgs.yml", 'r'))
-  end
-
-  def generateImageUrls targets
-    targets['list'].each do |target|
-      target.merge!({'icon': ActionController::Base.helpers.image_url(target['icon'])})
-    end
-
-    targets
   end
 
   def load_charts_data
