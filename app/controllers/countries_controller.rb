f72e5f2f1d42f72f56bf3859ba41530d8c48e46c
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-10-24 09:53:01 +0100

    Add comment to help making a decision on navigation

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,4 +1,10 @@
 class CountriesController < ApplicationController
   def index
+    #TODO
+    #If there is no other countries related page
+    #we could pull the relevant information from the YML file here.
+    #If we would like to have a url like /countries/ITA for each specific country
+    #we could add an extra action passing the ISO code for that country
+    #and fetch the relevant information from there
   end
 end
