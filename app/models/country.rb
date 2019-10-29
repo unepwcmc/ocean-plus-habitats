2bd8a4924a3739065884a9a43c609c51afd95fb9
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-10-29 14:41:07 +0000

    Add stats associations to country and habitat

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,7 @@
 class Country < ApplicationRecord
+  has_many :static_stat
+  # At the moment, only mangroves have got change stats,
+  # which means there can only be one change_stat record per country.
+  # This can change in the future
+  has_one :change_stat
 end
