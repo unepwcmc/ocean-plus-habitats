4c23f17fe1562a297f7e3f934d91c1e51754c551
Andrew Potter <hello@drewpotter.com>
2019-11-05 14:47:58 +0000

    Add scoping for countries and regions to geo_entity

--- app/models/geo_entity.rb
+++ app/models/geo_entity.rb
@@ -4,4 +4,7 @@ class GeoEntity < ApplicationRecord
   # which means there can only be one change_stat record per country.
   # This can change in the future
   has_one :change_stat
+
+  scope :countries, -> { where.not(iso3: nil) }
+  scope :regions, -> { where(iso3: nil) }
 end
