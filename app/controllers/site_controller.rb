79428ea4e6de25ab507f5efc3717d7c1560fba20
Andrew Potter <hello@drewpotter.com>
2019-11-07 14:46:29 +0000

    Remove load charts data method and comment out sql code for now

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,6 @@
 class SiteController < ApplicationController
   respond_to? :json, :html
   before_action :load_habitat
-  before_action :load_charts_data
 
   def index
     @global = YAML.load(File.open("#{Rails.root}/lib/data/content/global.yml", 'r'))
@@ -11,6 +10,43 @@ class SiteController < ApplicationController
     @habitatData = HabitatsSerializer.new(@habitat, @chart_greatest_coverage, @chart_protected_areas, @global).serialize
     
     @habitats = I18n.t('global.habitats')
+
+  #   query = <<-SQL
+  #   SELECT DISTINCT geo_entities.id AS geo_entities_id,
+  #                   geo_entities.iso3 AS iso3,
+  #                   geo_entities.name AS name,
+  #                   geo_entity_stats.habitat_id as habitat_id,
+  #                   geo_entity_stats.total_value AS total_value
+
+  #                   FROM geo_entities
+  #                   INNER JOIN geo_entity_stats ON geo_entities.id = geo_entity_stats.geo_entity_id
+  #                   INNER JOIN habitats ON habitats.id = geo_entity_stats.habitat_id
+  #                   WHERE geo_entities.iso3 IS NOT NULL
+  #                   AND habitat_id = #{@habitat.id}
+  #                   ORDER BY total_value DESC
+
+  #                   SQL
+
+  #          #GROUP BY geo_entities.id, geo_entities.iso3, geo_entities.name, geo_entity_stats.total_value
+
+  # geo_entity_stats = ActiveRecord::Base.connection.execute(query)
+
+  # puts geo_entity_stats.inspect
+  # geo_entity_stats.first(5).each do |geo_entity_stat|
+  #   puts geo_entity_stat.inspect
+  # end
+  # top_five_countries = geo_entity_stats.first(5)
+  # # top_five_countries = GeoEntity.countries.geo_entity_stat.where(habitat_id: @habitat.id)
+  # #                                .order('total_value DESC')
+  # #                                .first(5)
+  # arbitrary_value = top_five_countries.first["total_value"].to_f * 1.05
+  # @chart_greatest_coverage = top_five_countries.map do |stat|
+  #   {
+  #     label: stat["name"],
+  #     value: stat["total_value"].to_f.round(0),
+  #     percent: 100*stat["total_value"].to_f/arbitrary_value
+  #   }
+  # end
 
     #----------------------------------------------------------------------------#
     # FERDI variables will need adding to this object ---------------------------#
@@ -48,57 +84,5 @@ class SiteController < ApplicationController
     @habitat = Habitat.where(name: params['habitat'] || 'warmwater').first
     @habitat ||= Habitat.where(name: 'coralreefs').first
     @habitat_type = @habitat.type
-  end
-
-  def load_charts_data
-
-    query = <<-SQL
-      SELECT DISTINCT geo_entities.id AS geo_entities_id,
-                      geo_entities.iso3 AS iso3,
-                      geo_entities.name AS name,
-                      geo_entity_stats.habitat_id as habitat_id,
-                      geo_entity_stats.total_value AS total_value
-
-                      FROM geo_entities
-                      INNER JOIN geo_entity_stats ON geo_entities.id = geo_entity_stats.geo_entity_id
-                      INNER JOIN habitats ON habitats.id = geo_entity_stats.habitat_id
-                      WHERE geo_entities.iso3 IS NOT NULL
-                      AND habitat_id = #{@habitat.id}
-                      ORDER BY total_value DESC
-
-                      SQL
-
-             #GROUP BY geo_entities.id, geo_entities.iso3, geo_entities.name, geo_entity_stats.total_value
-
-    geo_entity_stats = ActiveRecord::Base.connection.execute(query)
-
-    puts geo_entity_stats.inspect
-    geo_entity_stats.first(5).each do |geo_entity_stat|
-      puts geo_entity_stat.inspect
-    end
-    top_five_countries = geo_entity_stats.first(5)
-    # top_five_countries = GeoEntity.countries.geo_entity_stat.where(habitat_id: @habitat.id)
-    #                                .order('total_value DESC')
-    #                                .first(5)
-    arbitrary_value = top_five_countries.first["total_value"].to_f * 1.05
-    @chart_greatest_coverage = top_five_countries.map do |stat|
-      {
-        label: stat["name"],
-        value: stat["total_value"].to_f.round(0),
-        percent: 100*stat["total_value"].to_f/arbitrary_value
-      }
-    end
-
-    #top_five_country_ids = top_five_countries.map(&:geo_entities_id)
-    # top_five_protected_areas = CountryStat.where(habitat: @habitat, country_id: top_five_country_ids)
-    #                                      .order("protected_percentage DESC")
-    #                                      .first(5)
-
-    # @chart_protected_areas = top_five_protected_areas.map do |stat|
-    #   {
-    #     label: stat.country.name,
-    #     percent: stat.protected_percentage.to_f.round(1),
-    #   }
-    # end
   end
 end
