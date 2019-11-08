848ef5e1b6fed84abc5fe5c7f9780d49a319adb3
Andrew Potter <hello@drewpotter.com>
2019-11-08 10:33:10 +0000

    Remove sql query

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -11,43 +11,6 @@ class SiteController < ApplicationController
     
     @habitats = I18n.t('global.habitats')
 
-  #   query = <<-SQL
-  #   SELECT DISTINCT geo_entities.id AS geo_entities_id,
-  #                   geo_entities.iso3 AS iso3,
-  #                   geo_entities.name AS name,
-  #                   geo_entity_stats.habitat_id as habitat_id,
-  #                   geo_entity_stats.total_value AS total_value
-
-  #                   FROM geo_entities
-  #                   INNER JOIN geo_entity_stats ON geo_entities.id = geo_entity_stats.geo_entity_id
-  #                   INNER JOIN habitats ON habitats.id = geo_entity_stats.habitat_id
-  #                   WHERE geo_entities.iso3 IS NOT NULL
-  #                   AND habitat_id = #{@habitat.id}
-  #                   ORDER BY total_value DESC
-
-  #                   SQL
-
-  #          #GROUP BY geo_entities.id, geo_entities.iso3, geo_entities.name, geo_entity_stats.total_value
-
-  # geo_entity_stats = ActiveRecord::Base.connection.execute(query)
-
-  # puts geo_entity_stats.inspect
-  # geo_entity_stats.first(5).each do |geo_entity_stat|
-  #   puts geo_entity_stat.inspect
-  # end
-  # top_five_countries = geo_entity_stats.first(5)
-  # # top_five_countries = GeoEntity.countries.geo_entity_stat.where(habitat_id: @habitat.id)
-  # #                                .order('total_value DESC')
-  # #                                .first(5)
-  # arbitrary_value = top_five_countries.first["total_value"].to_f * 1.05
-  # @chart_greatest_coverage = top_five_countries.map do |stat|
-  #   {
-  #     label: stat["name"],
-  #     value: stat["total_value"].to_f.round(0),
-  #     percent: 100*stat["total_value"].to_f/arbitrary_value
-  #   }
-  # end
-
     #----------------------------------------------------------------------------#
     # FERDI variables will need adding to this object ---------------------------#
     # https://guides.rubyonrails.org/i18n.html#passing-variables-to-translations #
