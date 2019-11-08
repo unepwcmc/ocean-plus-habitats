c97856d91b303d4030b5050ea953689139d40b3e
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-11-08 12:01:09 +0000

    Add join table for species and geo entities

--- app/models/geo_entity.rb
+++ app/models/geo_entity.rb
@@ -1,4 +1,6 @@
 class GeoEntity < ApplicationRecord
+  has_many :geo_entities_species, class_name: 'GeoEntitiesSpecies'
+  has_many :species, through: :geo_entities_species
   has_one :geo_entity_stat
   # At the moment, only mangroves have got change stats,
   # which means there can only be one change_stat record per country.
