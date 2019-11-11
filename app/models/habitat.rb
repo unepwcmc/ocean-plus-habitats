4f116269113051f1199307ed3acbb0f776f53268
Andrew Potter <hello@drewpotter.com>
2019-11-11 13:30:29 +0000

    Add calculate global cover change method using sql

--- app/models/habitat.rb
+++ app/models/habitat.rb
@@ -11,9 +11,60 @@ class Habitat < ApplicationRecord
     "Percentage of #{title.downcase} that occur within a marine protected area"
   end
 
-  def calculate_global_coverage
-    coverage = Carto.new(name).total_area.first['sum']
-    update_attributes(global_coverage: coverage.round)
+  # def calculate_global_coverage
+  #   # coverage = Carto.new(name).total_area.first['sum']
+  #   # update_attributes(global_coverage: coverage.round)
+
+
+  #   query = <<-SQL
+  #     SELECT geo_entities.id AS geo_entities_id,
+  #            geo_entities.iso3 AS iso3,
+  #            geo_entities.name AS geo_entities_name,
+  #            geo_entity_stats.habitat_id as habitat_id,
+  #            geo_entity_stats.total_value AS total_value,
+  #            SUM(geo_entity_stats.total_value) AS sum_total_value
+
+  #            FROM geo_entities
+  #            INNER JOIN geo_entity_stats ON geo_entities.id = geo_entity_stats.geo_entity_id
+  #            INNER JOIN habitats ON habitats.id = geo_entity_stats.habitat_id
+  #            WHERE geo_entities.iso3 IS NOT NULL
+  #            AND habitat_id = #{self.id}
+  #            GROUP BY sum_total_value
+
+  #            SQL
+
+  #   geo_entity_stats = ActiveRecord::Base.connection.execute(query)
+
+  #   puts geo_entity_stats.inspect
+  #   geo_entity_stats.first(5).each do |geo_entity_stat|
+  #     puts geo_entity_stat.inspect
+  #   end
+  # end
+
+  def calculate_global_cover_change
+    query = <<-SQL
+      SELECT geo_entities.id AS geo_entities_id,
+             geo_entities.iso3 AS iso3,
+             geo_entities.name AS geo_entities_name,
+             change_stats.habitat_id AS habitat_id,
+             SUM(change_stats.total_value_2010) - SUM(change_stats.total_value_1996) AS total_value_change
+
+             FROM geo_entities
+             INNER JOIN change_stats ON geo_entities.id = change_stats.geo_entity_id
+             INNER JOIN habitats ON habitats.id = change_stats.habitat_id
+             WHERE geo_entities.iso3 IS NOT NULL
+             AND habitat_id = #{self.id}
+             GROUP BY geo_entities_id, habitat_id
+             ORDER BY total_value_change DESC
+
+             SQL
+
+    geo_entity_stats = ActiveRecord::Base.connection.execute(query)
+
+    puts geo_entity_stats.inspect
+    geo_entity_stats.first(5).each do |geo_entity_stat|
+      puts geo_entity_stat.inspect
+    end
   end
 
   def total_value_by_country
