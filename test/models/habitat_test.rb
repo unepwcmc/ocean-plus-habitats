a7f9a7fd6eeb5fe998513bfeaa8242bc11070c48
Andrew Potter <hello@drewpotter.com>
2019-11-19 13:23:27 +0000

    Add failing tests

---
+++ test/models/habitat_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class HabitatTest < ActiveSupport::TestCase
+  test "calculate_country_cover_change" do
+    assert_equal true, false
+  end
+
+  test "calculate_global_cover_change" do
+    assert_equal true,false
+  end
+end
