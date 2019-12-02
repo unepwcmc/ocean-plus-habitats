1ee6a19ffb1e020d6967982e48af1824273bf521
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-12-02 22:43:25 +0000

    [wip] Quick fix for regions exception and data retrieval

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def site_title 
+  def site_title
     'The first authoritative online resource on marine habitats'
   end
 
@@ -7,11 +7,11 @@ module ApplicationHelper
     'Ocean+ Habitats is an evolving tool that provides insight into the known extent, protection and other statistics of ecologically and economically important ocean habitats, such as corals, mangroves, seagrasses and saltmarshes.'
   end
 
-  def title_meta_tag  
-    "Ocean+ Habitats" 
+  def title_meta_tag
+    "Ocean+ Habitats"
   end
 
- def url_encode text
+  def url_encode text
     ERB::Util.url_encode(text)
   end
 
@@ -27,33 +27,27 @@ module ApplicationHelper
     'Guanaco Torres del Paine Chile Gregoire Dubois'
   end
 
+  COUNTRIES = %w(indonesia united_arab_emirates).freeze
+  REGIONS = %w(mediterranean wider_caribbean).freeze
   def get_nav_items
     #FERDI - get all countries and then fill out object as follows
-    nav_items = {
-      countries: [
-        {
-          title: I18n.t("countries.indonesia.title"),
-          url: country_path(105)
-        },
-        {
-          title: I18n.t("countries.united_arab_emirates.title"),
-          url: country_path(234)
-        }
-      ],
-      regions: [
-        {
-          title: I18n.t("countries.mediterranean.title"),
-          url: country_path(257)
-        },
-        {
-          title: I18n.t("countries.wider_caribbean.title"),
-          url: country_path(267)
-        }
-      ]
+    {
+      countries: COUNTRIES.map { |c| nav_item(c) },
+      regions: REGIONS.map { |r| nav_item(r) }
     }
   end
-  
-  def get_habitat_icon_class (habitat, status='')
+
+  def nav_item(item)
+    geo_entity_name = item.humanize.split(' ').map(&:capitalize).join(' ')
+    geo_entity_id = GeoEntity.find_by_name(geo_entity_name)&.id
+    return {} unless geo_entity_id
+    {
+      title: I18n.t("countries.#{item}.title"),
+      url: country_path(geo_entity_id)
+    }
+  end
+
+  def get_habitat_icon_class(habitat, status='')
     status = status == 'present' || status == '' ? '' : "-#{status}"
 
     "icon--#{habitat}#{status}"
