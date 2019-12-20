c50d7ff5c30aa639f021afe06fcc99fa13c50642
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-12-20 14:08:50 +0000

    Use strings instead of symbols for consistency

--- app/models/habitat.rb
+++ app/models/habitat.rb
@@ -70,13 +70,13 @@ class Habitat < ApplicationRecord
   end
 
   def global_protection
-    stats = { name: name, total_value: 0, protected_value: 0 }
+    stats = { 'name' => name, 'total_value' => 0, 'protected_value' => 0 }
     geo_entity_stats.country_stats.map do |stat|
-      stats[:total_value] = stats[:total_value] + stat.total_value
-      stats[:protected_value] = stats[:protected_value] + stat.protected_value
+      stats['total_value'] = stats['total_value'] + stat.total_value
+      stats['protected_value'] = stats['protected_value'] + stat.protected_value
     end
-    protected_value = stats[:protected_value] > 0 ? stats[:protected_value] : 1
-    stats.merge({protected_percentage: stats[:total_value] / protected_value})
+    protected_value = stats['protected_value'] > 0 ? stats['protected_value'] : 1
+    stats.merge({'protected_percentage' => stats['total_value'] / protected_value})
   end
 
   def self.global_protection
