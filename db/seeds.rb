d4645a9e5737a1649a54783552d29738afd3de71
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2020-01-09 15:33:26 +0000

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,6 @@ end
 #  Rails.logger.info("Global coverage calculate for #{habitat.name}!")
 #end
 
-#Import countries
-count = 0
-CSV.foreach('lib/data/countries.csv', headers: true) do |row|
-  name, iso2, iso3 = [row['name'], row['alpha-2'], row['alpha-3']]
-  next if GeoEntity.where(name: name).first
-  count += 1 if GeoEntity.create(name: name, iso2: iso2, iso3: iso3)
-end
-Rails.logger.info("#{count} countries were created successfully!")
-
-%w(regions prebakedstats redlist_data).each do |import_type|
+%w(countries regions prebakedstats redlist_data).each do |import_type|
   Rake::Task["import:#{import_type}"].invoke
 end
