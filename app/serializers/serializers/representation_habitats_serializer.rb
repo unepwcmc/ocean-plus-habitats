85b951d17bb20866354b8930a9c46df8100434ff
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2020-03-20 17:21:17 +0000

    Refactor serializer a bit

--- app/serializers/serializers/representation_habitats_serializer.rb
+++ app/serializers/serializers/representation_habitats_serializer.rb
@@ -6,30 +6,59 @@ class Serializers::RepresentationHabitatsSerializer < Serializers::Base
 
   def serialize
     return {} if @country.iso3.nil?
-    coastal = CoastalStat.find_by(geo_entity_id: @country)
-                         .attributes
-                         .slice('total_coast_length', 'multiple_habitat_length')
-    tot = coastal['total_coast_length']
-    hab_stats = GeoEntityStat.where(geo_entity_id: @country)
-    legend, rows = [], []
-    Habitat.where.not(name: 'coldcorals').each_with_index do |habitat, label|
-      stat = hab_stats.find_by(habitat_id: habitat)
-      value = stat.try(:coastal_coverage) || 0
-      legend << { id: habitat.name, title: habitat.title }
-      rows << { percent: ((value / tot) * 100).round, label: "#{label + 2}." }
-    end
-    legend.unshift(id: 'multiple', title: 'Multiple habitats')
-    rows.unshift(percent: ((coastal['multiple_habitat_length'] / tot) * 100).round, label: '1.')
-    legend.push(id: 'notcovered', title: 'Not covered')
-    rows.push(percent: 100 - rows.map { |row| row[:percent] }.inject(0, :+), label: '6.')
-    res = {
-      legend: legend,
+    @coastal_stat = CoastalStat.find_by(geo_entity_id: @country)
+
+
+    @geo_stat = GeoEntityStat.where(geo_entity_id: @country)
+    @legend, @rows = [], []
+
+    add_habitats_stats
+    add_multiple_habitats
+    add_not_covered
+
+    {
+      legend: @legend,
       chart: {
-        chart_title: "% of length of coastline: #{tot.round} km",
+        chart_title: "% of length of coastline: #{@coastal_stat.total_coast_length.round} km",
         theme: "habitats",
-        rows: rows
+        rows: @rows
       }
     }
-    res
+  end
+
+  private
+
+  def add_habitats_stats
+    Habitat.where.not(name: 'coldcorals').each_with_index do |habitat, idx|
+      habitat_stat = @geo_stat.find_by(habitat_id: habitat)
+      value = habitat_stat.coastal_coverage || 0
+
+      @legend << { id: habitat.name, title: habitat.title }
+      @rows << row(value, @coastal_stat.total_coast_length, idx)
+    end
+  end
+
+  def add_multiple_habitats
+    @legend.unshift(id: 'multiple', title: 'Multiple habitats')
+    @rows.unshift(
+      row(
+        @coastal_stat.multiple_habitat_length,
+        @coastal_stat.total_coast_length,
+        1
+      )
+    )
+  end
+
+  def add_not_covered
+    @legend.push(id: 'notcovered', title: 'Not covered')
+    percent_value = 100 - @rows.map { |row| row[:percent] }.inject(0, :+)
+    @rows.push({percent: percent_value , label: '6.'})
+  end
+
+  def row(value, total_length, idx)
+    {
+      percent: ((value / total_length) * 100).round,
+      label: "#{idx + 2}."
+    }
   end
 end
