8ae020d719ef84e7cdbb1428392976c108fcc727
Stanley Liu <sliu1993@hotmail.co.uk>
2020-05-06 18:12:37 +0100

    as above

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -31,10 +31,6 @@ class CountriesController < ApplicationController
       @example_species_selected = @example_species_select[0].to_json
     end
 
-    # @example_species_selected = @example_species_select.find { |habitat|
-    #  habitat[:id] ==  ( 'mangroves' || 'seagrasses' || 'saltmarshes' || 'coralreefs' || 'coldcorals' )
-    # }.to_json
-
     @habitat_change = Serializers::HabitatCountryChangeSerializer.new(@country, habitats_present_status).serialize
 
     @stacked_row_chart = Serializers::RepresentationHabitatsSerializer.new(@country).serialize
