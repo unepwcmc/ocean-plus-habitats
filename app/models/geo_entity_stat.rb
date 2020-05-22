33a20942821fffb8202e12d6197a1dcca9c9dd8d
lucacug <luca.cugliari@unep-wcmc.org>
2020-05-22 17:37:39 +0100

    add stas csv generation method

--- app/models/geo_entity_stat.rb
+++ app/models/geo_entity_stat.rb
@@ -13,4 +13,15 @@ class GeoEntityStat < ApplicationRecord
     'seagrasses' => 'unknown',
     'coldcorals' => 'unknown'
   }
+
+  def self.to_csv
+    require 'csv'
+    attributes = %w[Country Habitat Total_value(km^2) Protected_value(km^2) Protected_percentage Coastal_coverage(km)]
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+      all.order(:geo_entity_id, :habitat_id).each do |ges|
+        csv << [ges.geo_entity.name, ges.habitat.name, ges.total_value, ges.protected_value, ges.protected_percentage, ges.coastal_coverage]
+      end
+    end
+  end
 end
