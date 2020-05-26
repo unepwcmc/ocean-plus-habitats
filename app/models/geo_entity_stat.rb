c84c791e29d531f322854c53f2fb60cc9cbe4883
lucacug <luca.cugliari@unep-wcmc.org>
2020-05-26 12:58:48 +0100

    add iso3 column to csv download

--- app/models/geo_entity_stat.rb
+++ app/models/geo_entity_stat.rb
@@ -16,11 +16,11 @@ class GeoEntityStat < ApplicationRecord
 
   def self.to_csv
     require 'csv'
-    attributes = %w[Country Habitat Total_value(km^2) Protected_value(km^2) Protected_percentage Coastal_coverage(km)]
+    attributes = %w[ISO3 Country Habitat Total_value(km^2) Protected_value(km^2) Protected_percentage Coastal_coverage(km)]
     CSV.generate(headers: true) do |csv|
       csv << attributes
       all.order(:geo_entity_id, :habitat_id).each do |ges|
-        csv << [ges.geo_entity.name, ges.habitat.name, ges.total_value, ges.protected_value, ges.protected_percentage, ges.coastal_coverage]
+        csv << [ges.geo_entity.iso3, ges.geo_entity.name, ges.habitat.name, ges.total_value, ges.protected_value, ges.protected_percentage, ges.coastal_coverage]
       end
     end
   end
