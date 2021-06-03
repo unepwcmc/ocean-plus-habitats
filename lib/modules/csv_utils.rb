b65af7da3ed02b558437d902ef4003ff638dff39
Stanley Liu <sliu1993@hotmail.co.uk>
2021-06-03 09:32:41 +0100

    fix: separate out CSV for mangroves from the rest of habitats and place each CSV into a folder named via ISO3

--- lib/modules/csv_utils.rb
+++ lib/modules/csv_utils.rb
@@ -4,7 +4,7 @@ module CsvUtils
   extend self
 
   # Creates a CSV from an array of CSV::Row objects
-  def csv_from_csv_rows(output_directory, filename, headers, csv_row_array)
+  def csv_from_csv_rows(output_directory, headers, csv_row_array, filename:)
     CSV.open("#{output_directory}/#{filename}.csv", 'wb') do |csv|
       csv << headers
       
