c3f41c7a3d62e6f0245e8c532d06e5e7af5cd81c
Will Kocur <will.kocur@unep-wcmc.org>
2021-06-07 18:20:37 +0100

    update content and add logo to about page

--- app/helpers/about_helper.rb
+++ app/helpers/about_helper.rb
@@ -5,13 +5,19 @@ module AboutHelper
         id: 'redlist',
         name: t('about.contributors.logos.red_list'),
         url: 'https://www.iucnredlist.org/',
-        image: 'redlist'
+        image: 'redlist.svg'
       },
       {
         id: 'protected-planet',
         name: t('about.contributors.logos.protected_planet'),
         url: 'https://www.protectedplanet.net/',
-        image: 'protected-planet'
+        image: 'protected-planet.svg'
+      },
+      {
+        id: 'global-mangrove-watch',
+        name: t('about.contributors.logos.global_mangrove_watch'),
+        url: 'https://www.globalmangrovewatch.org/',
+        image: 'global-mangrove-watch.png'
       }
     ]
   end
