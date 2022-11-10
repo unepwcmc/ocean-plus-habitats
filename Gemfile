c2ee6a123a7e656094b3f9f09fff465802186eb6
sergio <s.marrocoli@gmail.com>
2022-11-10 16:51:26 +0000

    feat: Add species status to api response

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ gem 'httparty', '~> 0.16.2'
 gem 'rubyzip', '>= 1.0.0' # will load new rubyzip version
 gem 'zip-zip' # will load compatibility for old rubyzip API.
 
+gem 'will_paginate'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -1,20 +1,18 @@
 class Api::V1::CountriesController < Api::V1::BaseController
   before_action :set_country, only: [:show]
+  before_action :set_countries, only: [:index]
 
   def show
     render json: @country.as_json(
       only: %i[name iso3],
-      methods: [:protected_area_statistics]
+      methods: %i[protected_area_statistics species_status]
     )
   end
 
   def index
-    @countries = GeoEntity.countries
-      .eager_load(geo_entity_stats: :habitat)
-
     render json: @countries.as_json(
       only: %i[name iso3],
-      methods: [:protected_area_statistics]
+      methods: %i[protected_area_statistics]
     )
   end
 
@@ -24,5 +22,12 @@ class Api::V1::CountriesController < Api::V1::BaseController
     @country = GeoEntity.countries
       .includes(geo_entity_stats: :habitat)
       .find_by(iso3: params[:iso3])
+  end
+
+  def set_countries
+    @countries = GeoEntity.countries
+      .order(name: :asc)
+      .paginate(page: params[:page] || 1, per_page: 25)
+      .eager_load(geo_entity_stats: :habitat)
   end
 end
