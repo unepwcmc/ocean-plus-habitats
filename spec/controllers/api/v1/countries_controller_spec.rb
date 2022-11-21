06f82a52afd32d0fb6cb76cc1752aa3df2d2286c
Will Kocur <willkocur@gmail.com>
2022-11-21 11:13:37 +0000

    feat: add pagination test to countries api endpoint

--- spec/controllers/api/v1/countries_controller_spec.rb
+++ spec/controllers/api/v1/countries_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
+DEFAULT_PER_PAGE = 25
+INDEX_COUNTRY_COUNT = 40
+
 RSpec.describe Api::V1::CountriesController, type: :controller do
   describe 'GET #show' do
     let!(:country) { create_list(:country, 2) }
@@ -14,14 +17,31 @@ RSpec.describe Api::V1::CountriesController, type: :controller do
   end
 
   describe 'GET #index' do
-    let!(:country) { create_list(:country, 2) }
+    let!(:country) { create_list(:country, INDEX_COUNTRY_COUNT) }
   
     it 'returns summary data for countries' do
       get :index, format: :json
       expect(response).to be_successful
       
       response_body = JSON.parse(response.body)
-      expect(response_body.pluck('iso3')).to match(GeoEntity.pluck(:iso3))
+      country_isos_first_page = GeoEntity.order(name: :asc)
+        .limit(DEFAULT_PER_PAGE)
+        .pluck(:iso3)
+
+      expect(response_body.pluck('iso3')).to match(country_isos_first_page)
+    end
+
+    it 'paginates summary data for countries' do
+      get :index, format: :json, params: { page: 2 }
+      expect(response).to be_successful
+
+      response_body = JSON.parse(response.body)
+      first_second_page_country = GeoEntity.order(name: :asc)
+        .all[DEFAULT_PER_PAGE]
+
+
+      expect(response_body.count).to match(INDEX_COUNTRY_COUNT - DEFAULT_PER_PAGE)
+      expect(response_body[0]['name']).to match(first_second_page_country.name)
     end
   end
 end
